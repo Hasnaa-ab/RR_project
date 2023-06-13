3e5ad409259b6412ef2ade84de3c96e72eca409e
askovr0n <artur.skowronski6@gmail.com>
2023-06-14 01:09:50 +0200

    Fixing functions and codes

--- textcorpus_reproduced.R
+++ textcorpus_reproduced.R
@@ -1,36 +1,29 @@
-  # Each comment is a .ipynb cell
-  # Install and load necessary packages
-  
-  
-  #install.packages(c("tm", "SnowballC", "slam", "topicmodels", "quanteda", "caret", "e1071", "randomForest", "kernlab", "cluster", "topicmodels", "LDAvis", "ggplot2", 'rlang', 'ranger))
-  
-  library(tm)
-  library(slam)
-  library(quanteda)
-  library(caret)
-  library(e1071)
-  library(randomForest)
-  library(kernlab)
-  library(cluster)
-  library(topicmodels)
-  library(LDAvis)
-  library(ggplot2)
-  library(stringr)
-  library(tokenizers)
-  library(SnowballC)
-  library(ranger)
-  library(Matrix)
-
-  # Load data
-  fulldata <- read.csv("fulldata-updated.csv")
-  fulldata <- fulldata[!duplicated(fulldata$title), ]
-  fulldata <- fulldata[order(rownames(fulldata)), ]
-  fulldata <- as.data.frame(lapply(fulldata, type.convert))
-  rownames(fulldata) <- NULL
+# Each comment is a .ipynb cell
+# Install and load necessary packages
 
 
-  ################## function to read txt and return data frame
-  multiTextFile <- function(directoryPath) {
+#install.packages(c("tm", "SnowballC", "slam", "topicmodels", "quanteda", "caret", "e1071", "randomForest", "kernlab", "cluster", "topicmodels", "LDAvis", "ggplot2", 'rlang', 'ranger'))
+
+library(tm)
+library(slam)
+library(quanteda)
+library(caret)
+library(e1071)
+library(randomForest)
+library(kernlab)
+library(cluster)
+library(topicmodels)
+library(LDAvis)
+library(ggplot2)
+library(stringr)
+library(tokenizers)
+library(SnowballC)
+library(ranger)
+library(Matrix)
+
+
+################## function to read txt and return data frame
+multiTextFile <- function(directoryPath) {
   # Get the list of file names in the directory
   fileNames <- list.files(path = directoryPath, pattern = "\\.txt$", full.names = TRUE)
   
@@ -53,53 +46,66 @@
   
   # Return the merged data frame
   return(mergedData)
-  }
+}
+
+############################# function to read csv with text
+
+csvText <- function(file, textCol, labelCol) {
   
-  ######################### merge label col to data frame
+  data <- read.csv(file)
+  data = data[,c(textCol,labelCol)]
+  names(data) = c('text','label')
+  names(data[,labelCol]) = 'label'
+  data <- data[!duplicated(data$text), ]
+  data$text = as.character(data$text)
+  data$label = as.factor(data$label)
   
-  assignLabels <- function(df,labels) {
-    
-    df <- cbind(df,labels)
-    names(df)[2] = 'label'
-    return(df)
-  }
+  return(data)
   
-  ##########################
+}
+myData = csvText('fulldata-updated.csv', 'title', 'label')
+
+
+
+######################### merge label col to data frame
+
+assignLabels <- function(df,labels) {
   
-  #
-  labelcount <- table(fulldata$label)
+  df <- cbind(df,labels)
+  names(df)[2] = 'label'
+  return(df)
+}
+
+######################drop unique labels
+
+validLabels <- function(df) {
+  labelcount <- table(df$label)
   repeated <- names(labelcount[labelcount > 1])
-  fulldata <- fulldata[fulldata$label %in% repeated, ]
-  fulldata <- droplevels(fulldata)  # Drop unused levels if needed
-  rownames(fulldata) <- seq_len(nrow(fulldata))
+  df <- df[df$label %in% repeated, ]
+  df <- droplevels(df)  # Drop unused levels if needed
+  rownames(df) <- seq_len(nrow(df))
+  return(df)
   
-  ######################drop unique labels
+}
+myData <- validLabels(myData)
 
-  validLabels <- function(df) {
-    labelcount <- table(df$label)
-    repeated <- names(labelcount[labelcount > 1])
-    df <- df[df$label %in% repeated, ]
-    df <- droplevels(df)  # Drop unused levels if needed
-    rownames(df) <- seq_len(nrow(df))
-    
-  }
+############################ cleaning Text
+
+cleanText <- function(data) {
   
-  ############################
+  data$text <- str_replace_all(data$text, "\n", " ")
+  data$text <- str_replace_all(data$text, "[0-9]+", "")
+  data$text <- str_replace_all(data$text, "[,\\!?/:;''()``’“-”—#]", "")
+  data$text <- str_replace_all(data$text, "[.]+", "")
+  data$text <- tolower(data$text)
+  data$text <- str_replace_all(data$text, "\\b\\w\\b", "")
+  data$text <- as.character(data$text)
+  
+  return(data$text)
+}
 
-#
-fulldata$date <- as.POSIXct(fulldata$date, format = "%Y-%m-%d %H:%M:%S")
-fulldata$label <- as.factor(fulldata$label)
-month_stats <- summary(as.numeric(format(fulldata$date, "%m")))
-fulldata$article <- as.character(fulldata$article)
-
-# Clean text
-articles <- str_replace_all(fulldata$article, "\n", " ")
-articles <- str_replace_all(articles, "[0-9]+", "")
-articles <- str_replace_all(articles, "[,\\!?/:;''()``’“-”—#]", "")
-articles <- str_replace_all(articles, "[.]+", "")
-articles <- tolower(articles)
-articles <- str_replace_all(articles, "\\b\\w\\b", "")
-articles <- as.character(articles)
+articles <- cleanText(myData)
+################################# create tokens
 
 # Tokenizer
 articles <- sapply(articles, function(x) tokenizers::tokenize_words(x))
@@ -135,50 +141,28 @@ dtm <- DocumentTermMatrix(Corpus(VectorSource(articles)))
 dtm_tfidf <- weightTfIdf(dtm)
 
 # Label encoding
-fulldata$labelnumber <- as.numeric(as.factor(fulldata$label))
+myData$labelnumber <- as.numeric(as.factor(myData$label))
 
 # Merge the labels with the dtm
-dtm_tfidf <- cbind(dtm_tfidf, fulldata$labelnumber)
+dtm_tfidf <- cbind(dtm_tfidf, myData$labelnumber)
 
 # Split the dataset into training and testing sets
 set.seed(4545) #seed for the reproducibility
-trainIndex <- createDataPartition(fulldata$labelnumber, p = .6, list = FALSE, times = 1)
+trainIndex <- createDataPartition(myData$labelnumber, p = .6, list = FALSE, times = 1)
 
 # Split the labels as well
-train_labels <- fulldata$labelnumber[trainIndex]
-test_labels  <- fulldata$labelnumber[-trainIndex]
+train_labels <- myData$labelnumber[trainIndex]
+test_labels  <- myData$labelnumber[-trainIndex]
 
 # Split the dtm_tfidf
 train <- dtm_tfidf[trainIndex,]
-test  <- dtm_tfidf[-trainIndex,]
-
-# Generate a document-term matrix
-dtm <- DocumentTermMatrix(Corpus(VectorSource(articles)))
-
-# Transform to a term frequency-inverse document frequency (TF-IDF) matrix
-dtm_tfidf <- weightTfIdf(dtm)
-
-# Label encoding
-fulldata$labelnumber <- as.numeric(as.factor(fulldata$label))
-
-# Merge the labels with the dtm
-dtm_tfidf <- cbind(dtm_tfidf, fulldata$labelnumber)
-
-# Split the dataset into training and testing sets
-set.seed(4545) #seed for the reproducibility
-trainIndex <- createDataPartition(fulldata$labelnumber, p = .6, list = FALSE, times = 1)
-
-# Split the labels as well
-train_labels <- fulldata$labelnumber[trainIndex]
-test_labels  <- fulldata$labelnumber[-trainIndex]
-
-# Split the dtm_tfidf
-train <- dtm_tfidf[trainIndex,]
-test  <- dtm_tfidf[-trainIndex,]
+test <- dtm_tfidf[-trainIndex,]
 
 # Prepare matrices suitable for Random Forest
 train_df <- as.data.frame(as.matrix(train))
 test_df <- as.data.frame(as.matrix(test))
+colnames(train_df)[ncol(train_df)] <- "labelnumer"
+colnames(test_df)[ncol(test_df)] <- "labelnumer"
 
 # Rename the columns to use only alphanumeric characters and underscores
 names(train_df) <- make.names(names(train_df), unique = TRUE)
@@ -200,28 +184,35 @@ table(predictions$predictions, test_labels)
 accuracy <- sum(round(predictions$predictions, 0)  == test_labels) / length(test_labels)
 
 # Run a Bagging model
-control <- trainControl(method = "cv", number = 2) # Changed method to 'cv' for cross-validation and number to 2 for 2-fold cross-validation, as it is computationally heavy.
-model_bag <- train(as.factor(train_labels) ~ ., data = train_df, trControl = control, method = "treebag")
-predictions_bag <- predict(model_bag, newdata = test_df)
 
+control <- trainControl(method = "cv", number = 2) # Changed method to 'cv' for cross-validation and number to 2 for 2-fold cross-validation, as it is computationally heavy.
+model_bag <- caret::train(labelnumer ~ ., data=train_df, trControl=control, method="treebag")
+predictions_bag <- predict(model_bag, newdata = test_df)
+length(myData$labelnumber)
 # Print classification report
-print(confusionMatrix(predictions_bag, test_df$labelnumber))
+
+# Number of levels is not the same, that's why it is not working
+# print(confusionMatrix(factor(test_labels, levels=1:11), as.factor(round(predictions_bag)), levels=1:11))
+class(train_df)
+
+library(dplyr)
 
 # Run a LDA model and plot the topics
-lda <- LDA(train_df[, !colnames(train_df) %in% "labelnumber"], k = 20, control = list(seed = 3434))
-topics <- tidy(lda, matrix = "beta")
+lda <- LDA(dtm, k = 20, control = list(seed = 3434))
+topics <- tidytext::tidy(lda, matrix = "beta")
 top_terms <- topics %>%
   group_by(topic) %>%
   top_n(10, beta) %>%
   ungroup() %>%
   arrange(topic, -beta)
 plot_lda <- top_terms %>%
-  mutate(term = reorder_within(term, beta, topic)) %>%
+  mutate(term = tidytext::reorder_within(term, beta, topic)) %>%
   ggplot(aes(beta, term, fill = factor(topic))) +
   geom_col(show.legend = FALSE) +
   facet_wrap(~ topic, scales = "free") +
-  coord_flip() +
+  # coord_flip() +
   theme_minimal() +
   labs(title = "Top 10 terms in each LDA topic",
        x = "Beta", y = "")
+
 print(plot_lda)
